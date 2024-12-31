d4d25ed7a97253912a0f731fc06497defab2ae4f
RoselynFrancis <rfrancis@st.ug.edu.gh>
2024-12-31 00:47:18 +0000

    Delete vehicleApp directory

--- vehicleApp/src/main/java/vehiclerental/interfaces/Rentable.java
+++
@@ -1,7 +0,0 @@
-package vehiclerental.interfaces;
-import vehiclerental.model.Customer;
-
-public interface Rentable {
-    boolean rent(Customer customer, int daysRented);
-    boolean returnVehicle();
-}
